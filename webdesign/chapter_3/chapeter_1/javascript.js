967caecb9e64e2e699b8f5bfad2b50c07de96f26
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2023-10-19 16:25:10 +0800

    マルチデバイス対応にCSSを変更

--- webdesign/chapter_3/chapeter_1/javascript.js
+++ webdesign/chapter_3/chapeter_1/javascript.js
@@ -1,3 +1,8 @@
-function refresh_jump() {
-    window.open("https://github.com/mintson0517/usc_html-css", "newwindow");
+function showConfirmation() {
+    var userConfirmation = confirm("このサイトのHTML、CSSコードがGitHub上で観覧できます");
+    if (userConfirmation) {
+        window.open("https://github.com/mintson0517/usc_html-css", "newwindow");
+    } else {
+        // ユーザーがキャンセルした場合、何もしない
+    }
 }
